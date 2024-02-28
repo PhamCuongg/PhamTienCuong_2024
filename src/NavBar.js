e593b1a6a11b2fc43cb00fb28d332aa9cc0053ae
PhamCuongg <90382552+PhamCuongg@users.noreply.github.com>
2024-02-28 14:15:58 +0700

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ const Navbar = ({ currentDate, searchTerm, handleSearch }) => {
   return (
     <div className="navbar">
       <div className="navbar-item">{currentDate}</div>
-      <div className="navbar-item homepage">HomePage</div>
+      <div className="navbar-item homepage">Blogs</div>
       <div className="navbar-item search-bar">
         <input
           type="text"
